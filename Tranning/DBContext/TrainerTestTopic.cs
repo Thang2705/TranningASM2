a723fe41634ed62f5b015fdf624a4461c98037f6
Your Name <you@example.com>
2023-12-09 20:59:10 +0700

    first commit

---
+++ Tranning/DBContext/TrainerTestTopic.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tranning.DBContext
+{
+    public class TrainerTestTopic
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("CourseId")]
+        public int CourseId { get; set; }
+        public TrainerTestCourse TrainerTestCourse { get; set; }  // reference navigation
+
+        [Column("NameTopic", TypeName = "Varchar(50)")]
+        public string NameTopic { get; set; }
+
+        [Column("Description", TypeName = "Varchar(200)")]
+        public string Description { get; set; }
+
+        [Column("Video", TypeName = "Varchar(200)")]
+        public string? Video { get; set; }
+        [Column("Document", TypeName = "Varchar(150)")]
+        public string Document { get; set; }
+        [Column("AttachFile", TypeName = "Varchar(150)")]
+        public string? AttachFile { get; set; }
+
+        [Column("Status", TypeName = "Integer")]
+        public int Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
